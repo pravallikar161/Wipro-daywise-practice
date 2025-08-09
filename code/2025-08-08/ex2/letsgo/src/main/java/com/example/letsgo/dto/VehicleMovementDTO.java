449c13457b2fa29a7de71f38d2e2f94a496f8d8a
chandrasekhar git <chandrasekharreddyram@gmail.com>
2025-08-09 09:38:34 +0530

    first commit

---
+++ code/2025-08-08/ex2/letsgo/src/main/java/com/example/letsgo/dto/VehicleMovementDTO.java
@@ -0,0 +1,31 @@
+package com.example.letsgo.dto;
+
+public class VehicleMovementDTO {
+    private int vehId;
+    private double lat;
+    private double longitude;
+
+    public int getVehId() {
+        return vehId;
+    }
+
+    public void setVehId(int vehId) {
+        this.vehId = vehId;
+    }
+
+    public double getLat() {
+        return lat;
+    }
+
+    public void setLat(double lat) {
+        this.lat = lat;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+}
