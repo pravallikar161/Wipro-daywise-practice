e87671cdc36e7d00a619aeb80fb9141f053aeeca
pravallikar161 <pravallikar161@gmail.com>
2025-08-09 10:20:33 +0530

    Delete code directory

--- code/2025-08-08/ex2/letsgo/src/main/java/com/example/letsgo/repository/VehicleMovementRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.letsgo.repository;
-
-import com.example.letsgo.entity.VehicleMovement;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VehicleMovementRepository extends JpaRepository<VehicleMovement, Long> {
-}
